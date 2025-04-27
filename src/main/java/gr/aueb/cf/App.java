c70c5be03c5f79b9e46ccc22190034252c767383
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-27 19:21:50 +0300

    feat: teacher created with transaction

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,9 +1,15 @@
 package gr.aueb.cf;
 
 
+import gr.aueb.cf.core.GenderType;
+import gr.aueb.cf.model.Region;
+import gr.aueb.cf.model.Teacher;
+import gr.aueb.cf.model.TeacherMoreInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.time.LocalDateTime;
 
 public class App {
     private final static EntityManagerFactory emf = Persistence.createEntityManagerFactory("school7PU");
@@ -13,18 +19,31 @@ public class App {
 
         // Here transaction starts (work unit)
 
-//        em.getTransaction().begin();
+        em.getTransaction().begin();
+
+        // Φτιάχνω τον Teacher με τον builder (που δημιουργήθηκε απο το Lombok)
+        Teacher teacher = Teacher.builder()
+                .firstname("Κώστας")
+                .lastname("Γιαννούτσος")
+                .isActive(true)
+                .build();
+
+        // Φτιάχνω το TeacherMoreInfo με τον builder (που δημιουργήθηκε απο το Lombok)
+        TeacherMoreInfo teacherMoreInfo = TeacherMoreInfo.builder()
+                .dateOfBirth(LocalDateTime.of(2000, 2, 1, 10, 10))
+                .gender(GenderType.MALE)
+                .build();
 
 
+        // Βάζω στον teacher που έφτιαξα το teacherMoreInfo, Region κλπ
+        teacher.setTeacherMoreInfo(teacherMoreInfo);
+        Region region = em.find(Region.class, 1);
+        teacher.addRegion(region);
 
-
-
-
-
-
+        em.persist(teacher); // Θα γίνει persist και το teacherMoreInfo
 
         // Here the transaction ends
-//        em.getTransaction().commit();
+        em.getTransaction().commit();
 
         // Closing the resources
         emf.close();
