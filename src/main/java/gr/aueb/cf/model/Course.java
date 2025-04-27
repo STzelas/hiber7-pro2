08045532c2437212f001f02aed057136eb0ca49e
Sot. Tzelatidis <stzelati@gmail.com>
2025-04-27 18:46:34 +0300

    feat: model for the app created with lombok and hibernate creating the java beans and tables/columns and relations

--- src/main/java/gr/aueb/cf/model/Course.java
+++ src/main/java/gr/aueb/cf/model/Course.java
@@ -1,4 +1,76 @@
 package gr.aueb.cf.model;
 
+import gr.aueb.cf.core.LessonType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "courses")
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+//    @Column(length = 1000000)
+    @Column(columnDefinition = "TEXT")
+    private String comments;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "lesson_type", columnDefinition = "TINYINT COMMENT 'ΕΙΔΟΣ ΜΑΘΗΜΑΤΟΣ (1. θεωρία, 2. Εργαστήριο, 3. Μικτό")
+    private LessonType lessonType;
+
+    @Getter(AccessLevel.PROTECTED)
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "courses_teachers")  // table-name + _id Αν τειρούνται τα conventions
+//    @JoinTable(name = "courses_teachers",
+//            joinColumns = @JoinColumn(name = "course_id", referencedColumnName = "id"),
+//            inverseJoinColumns = @JoinColumn(name = "teacher_id", referencedColumnName = "id"))
+    private Set<Teacher> teachers = new HashSet<>();
+
+
+    public Set<Teacher> getAllTeachers() {
+        return Collections.unmodifiableSet(teachers);
+    }
+
+    public void addTeacher(Teacher teacher) {
+        if (teachers == null) teachers = new HashSet<>();
+        teachers.add(teacher);
+        teacher.getCourses().add(this);
+    }
+
+    public void removeTeacher(Teacher teacher) {
+        if (teachers == null) return;
+        teachers.remove(teacher);
+        teacher.getCourses().remove(this);
+    }
+
+    public Course(Long id, String title, String comments, LessonType lessonType) {
+        this.id = id;
+        this.title = title;
+        this.comments = comments;
+        this.lessonType = lessonType;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", comments='" + comments + '\'' +
+                ", lessonType=" + lessonType +
+                '}';
+    }
 }
